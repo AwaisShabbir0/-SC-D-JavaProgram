c0a87cd544a82ceba4acc7e91af865bca73dac9b
AwaisShabbir0 <wasikamboh0810@gmail.com>
2025-10-07 15:23:19 +0500

    hello from git

--- helloword/src/HelloWorld.java
+++ helloword/src/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 		System.out.print("My Name is: Awais Shabbir");
+		System.out.print("Hello git");
     }
 }
    
